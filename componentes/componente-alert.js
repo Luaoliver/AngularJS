182035ce61defe50f54be92aebb2f46beb5cca0d
luaoliver <lulu-so@hotmail.com>
2021-04-29 15:12:30 -0300

    updating directive

--- componentes/componente-alert.js
+++ componentes/componente-alert.js
@@ -1,11 +1,21 @@
 angular.module('app').component('alertParaUsuario', {
     templateUrl :  'componentes/alert.html', //vai apontar para:
     bindings : { //variáveis que podem transacionar
-        msg : '=' // nome da variável : two way data binding
+        msg : '=', // nome da variável : two way data binding
+        tipo : '='
     },
     controllerAs : 'vm',
     controller : function(){
         vm = this
-        vm.teste = 'blá blá'
+        vm.tipoClasse = undefined
+
+        vm.$onInit = function(){ //$onInit para que inicie junto a tela principal
+            if(vm.tipo == 'sucesso'){
+                vm.tipoClasse = 'alert alert-success'
+            } else if (vm.tipo == 'erro'){
+                vm.tipoClasse = 'alert alert-danger'
+            }
+        }
+
     }
 })
